b22e999abf3cfd3f8f6edc78d99399b7248a6f05
maXe <max@copteruni.com>
2025-09-11 00:30:29 +0200

    Add inputYtVideoId

--- CodePen/cu-codepen.js
+++ CodePen/cu-codepen.js
@@ -4,6 +4,29 @@ const output = document.getElementById("imageSrcOutput");
 const cpyButton = document.querySelector('[data-cu-btn="cpy"]');
 const outNaturalWidth = document.getElementById("naturalWidth");
 
+const inputYtVideoId = document.getElementById("ytVideoId");
+const imgThumbnail = document.getElementById("thumbnail");
+
+let ytVideoId = inputYtVideoId.placeholder
+
+const YT_IMG_HOST_JPG = 'https://i.ytimg.com/vi/'
+function ytThumbUrl(id, variant = 'sddefault') {
+    if (!id) return ''
+    return `${YT_IMG_HOST_JPG}${encodeURIComponent(id)}/${variant}.jpg` // Init: ytThumbUrl(id, 'sddefault')
+}
+
+// Init
+imgThumbnail.src = ytThumbUrl(ytVideoId, 'maxresdefault')
+output.value = imgThumbnail.src;
+
+
+inputYtVideoId.addEventListener("input", function (event) {
+    imgThumbnail.src = ytThumbUrl(event.target.value, 'maxresdefault')
+    output.value = imgThumbnail.src;
+});
+
+//https://i.ytimg.com/vi_webp/POg49GumiVQ/maxresdefault.webp
+
 // Klick-Ereignis für den ersten Button
 button.addEventListener("click", function () {
     // Attributwert vom Button lesen
@@ -22,6 +45,8 @@ button.addEventListener("click", function () {
     console.log("Gefundenes Bild:", image);
     console.log("Angezeigte Breite:", image.width);
     console.log("Originalbreite:", image.naturalWidth);
+    console.log(ytDefaultId)
+    console.log(urlYT_IMG_HOST_JPG)
 });
 
 // Klick-Ereignis für den Copy-Button
