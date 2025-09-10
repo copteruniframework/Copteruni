9ceda02cfbace8c135c39ddfc8b5620d443607e6
maXe <max@copteruni.com>
2025-09-10 22:56:09 +0200

    Add cpy btn

--- CodePen/cu-codepen.js
+++ CodePen/cu-codepen.js
@@ -1,20 +1,29 @@
 // Button über Attribut finden
 const button = document.querySelector('[data-cu-btn="ex1"]');
 const output = document.getElementById("imageSrcOutput");
+const cpyButton = document.querySelector('[data-cu-btn="cpy"]');
 
-// Klick-Ereignis
+// Klick-Ereignis für den ersten Button
 button.addEventListener("click", function () {
     // Attributwert vom Button lesen
     const btnValue = button.dataset.cuBtn;   // z.B. "ex1"
 
-    // Bild mit gleichem Wert suchen
+    // Bild mit gleichem data-cu-id suchen
     const image = document.querySelector(`[data-cu-id="${btnValue}"]`);
 
+    // URL ins Input schreiben
     output.value = image.src;
 
-    // In der Konsole ausgeben
+    // In der Konsole Infos ausgeben
     console.log("Button-Attribut:", btnValue);
     console.log("Gefundenes Bild:", image);
-    console.log(image.width); // angezeigte Breite in Pixel
-    console.log(image.naturalWidth);  // originale Breite der Bilddatei
+    console.log("Angezeigte Breite:", image.width);
+    console.log("Originalbreite:", image.naturalWidth);
+});
+
+// Klick-Ereignis für den Copy-Button
+cpyButton.addEventListener("click", function () {
+    if (output.value) {
+        navigator.clipboard.writeText(output.value)
+    }
 });
