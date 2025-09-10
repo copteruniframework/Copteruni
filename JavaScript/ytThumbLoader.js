1d15945b122da62acfd9de989d332032e34f62ab
maXe <max@copteruni.com>
2025-09-10 13:46:56 +0200

    Add YT_VARIANTS

--- JavaScript/ytThumbLoader.js
+++ JavaScript/ytThumbLoader.js
@@ -1,3 +1,11 @@
+const YT_VARIANTS = [
+  { name: 'default',      width: 120 },
+  { name: 'mqdefault',    width: 320 },
+  { name: 'hqdefault',    width: 480 },
+  { name: 'sddefault',    width: 640 },
+  { name: 'maxresdefault',width: 1280 },
+];
+
 document.querySelectorAll('img[data-yt-id]').forEach(img => {
     const id = img.dataset.ytId
     img.src = `https://i.ytimg.com/vi/${id}/maxresdefault.jpg`
