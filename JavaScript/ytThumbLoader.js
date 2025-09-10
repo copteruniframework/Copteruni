04b9ba1cef166fb824c1cf687a62209df0c0bcc3
maXe <max@copteruni.com>
2025-09-10 13:25:56 +0200

    Edit yt src link

--- JavaScript/ytThumbLoader.js
+++ JavaScript/ytThumbLoader.js
@@ -1,6 +1,6 @@
 document.querySelectorAll('img[data-yt-id]').forEach(img => {
     const id = img.dataset.ytId
-    img.src = `https://img.youtube.com/vi/${id}/maxresdefault.jpg`
+    img.src = `https://i.ytimg.com/vi/${id}/maxresdefault.jpg`
     img.loading = 'lazy'
     img.decoding = 'async'
 })
