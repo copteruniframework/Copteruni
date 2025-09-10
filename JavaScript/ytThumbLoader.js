34d6cbc295afd46723e004a31d6871805fc05ec0
maXe <max@copteruni.com>
2025-09-10 15:14:40 +0200

    Delete console.log(`[ytThumb] id=${id} width=${width}px`) block

--- JavaScript/ytThumbLoader.js
+++ JavaScript/ytThumbLoader.js
@@ -22,11 +22,3 @@ document.querySelectorAll('img[data-yt-id]').forEach(img => {
 
     console.log(`[ytThumb] id=${id} width=${width}px`)
 })
-
-document.addEventListener('DOMContentLoaded', () => {
-    document.querySelectorAll('img[data-yt-id]').forEach(img => {
-        const id = img.dataset.ytId
-        const width = Math.round(img.getBoundingClientRect().width)
-        console.log(`[ytThumb] id=${id} width=${width}px`)
-    })
-})
