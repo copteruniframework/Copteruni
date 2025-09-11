aed683d3c9193e4256bb378d90df255152ea2168
maXe <max@copteruni.com>
2025-09-11 13:01:51 +0200

    Change all quey

--- CodePen/cu-codepen.js
+++ CodePen/cu-codepen.js
@@ -1,14 +1,9 @@
 // Button über Attribut finden
-const output = document.getElementById("imageSrcOutput");
-const cpyButton = document.querySelector('[data-cu-btn="cpy"]');
-const pNaturalWidth = document.getElementById("naturalWidth");
-const pThumbWidth = document.getElementById("Width");
-
-const inputYtVideoId = document.getElementById("ytVideoId");
-const imgThumbnail = document.getElementById("thumbnail");
-const imgThumbnailLabel = imgThumbnail.parentElement.querySelector('.image_text_label');
-
-const initVideoId = inputYtVideoId.placeholder
+const form_ytThumbLinkGen = document.getElementById("ytThumbLinkGen");
+const input_ytVideoId = form_ytThumbLinkGen.querySelector('input[name="ytVideoId"]');
+const img_ytThumbnail = form_ytThumbLinkGen.querySelector('img');
+const label_ytThumbnail = img_ytThumbnail.parentElement.querySelector('.image_text_label');
+const output_ytThumbUrl = form_ytThumbLinkGen.querySelector('input[name="ytThumbUrl"]');
 
 const YT_IMG_HOST_JPG = 'https://i.ytimg.com/vi/'
 const YT_IMG_HOST_WEBP = 'https://i.ytimg.com/vi_webp/'
@@ -27,25 +22,24 @@ function ytThumbUrl(id, variant = 'sddefault', format = "jpg") {
 
 function updateThum(id) {
     if (!id?.trim()) {
-        imgThumbnail.src = ytThumbUrl(initVideoId, 'maxresdefault')
+        img_ytThumbnail.src = ytThumbUrl(input_ytVideoId.placeholder, 'maxresdefault')
     } else {
-        imgThumbnail.src = ytThumbUrl(id.trim(), 'maxresdefault')
+        img_ytThumbnail.src = ytThumbUrl(id.trim(), 'maxresdefault')
     }
-    output.value = imgThumbnail.src;
-    imgThumbnail.onload = () => {
-        imgThumbnailLabel.textContent = `${imgThumbnail.naturalWidth} x ${imgThumbnail.naturalHeight}px`;
-        pNaturalWidth.textContent = `Originalbreite des Bildes: ${imgThumbnail.naturalWidth}px`;
-        pThumbWidth.innerHTML = `<strong>Angezeigte Breite: ${imgThumbnail.width}px</strong>`;
+    output_ytThumbUrl.value = img_ytThumbnail.src;
+    img_ytThumbnail.onload = () => {
+        label_ytThumbnail.textContent = `${img_ytThumbnail.naturalWidth} x ${img_ytThumbnail.naturalHeight}px`;
     };
 }
 
 // Init
 updateThum()
 
-inputYtVideoId.addEventListener("input", function (event) {
+input_ytVideoId.addEventListener("input", function (event) {
     updateThum(event.target.value)
 });
 
+const cpyButton = document.querySelector('[data-cu-btn="cpy"]');
 // Klick-Ereignis für den Copy-Button
 cpyButton.addEventListener("click", function () {
     if (output.value) {
