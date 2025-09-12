47132556d658f4f091c06b92a76025263542c312
maXe <max@copteruni.com>
2025-09-12 09:43:23 +0200

    Add cu-list-sort.js

--- CodePen/cu-codepen.js
+++ CodePen/cu-codepen.js
@@ -74,3 +74,17 @@ toggle.addEventListener('change', (e) => {
         root.removeAttribute('data-theme');       // zurück zu Dark (Default)
     }
 });
+
+document.addEventListener("DOMContentLoaded", () => {
+  const containers = document.querySelectorAll('[cu-list-sort="abc"]');
+
+  containers.forEach(container => {
+    const items = Array.from(container.children);
+
+    items.sort((a, b) =>
+      a.textContent.trim().localeCompare(b.textContent.trim(), "de", { sensitivity: "base" })
+    );
+
+    items.forEach(item => container.appendChild(item));
+  });
+});
